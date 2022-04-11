fb3dd7a87da4c927403338326543e985057ebf6f
TRAN VAN TAM <tam191203380@st.utc.edu.vn>
2022-04-12 01:20:55 +0700

    dsf

--- QuanLyKhachSan/Controllers/View/RoomsController.cs
+++ QuanLyKhachSan/Controllers/View/RoomsController.cs
@@ -13,7 +13,8 @@ namespace QuanLyKhachSan.Controllers
         // GET: Rooms
         public ActionResult ListRooms()
         {
-            IEnumerable<Phong> rooms = null;
+            IEnumerable<Room> rooms = null;
+            
             using(var client  = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://localhost:44370/api/v1/room/");
@@ -22,17 +23,18 @@ namespace QuanLyKhachSan.Controllers
                 var result = responseTack.Result;
                 if (result.IsSuccessStatusCode)
                 {
-                    var readRoom = result.Content.ReadAsAsync<IList<Phong>>();
+                    var readRoom = result.Content.ReadAsAsync<IList<Room>>();
                     readRoom.Wait();
                     rooms = readRoom.Result;
                 }
                 else
                 {
-                    rooms = Enumerable.Empty<Phong>();
+                    rooms = Enumerable.Empty<Room>();
                     ModelState.AddModelError(String.Empty, "Server erro");
                 }
 
             }
+
             return View(rooms);
         }
 
