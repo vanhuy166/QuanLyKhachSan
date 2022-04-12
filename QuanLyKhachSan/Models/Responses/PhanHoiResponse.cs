8434a36cabcfa4fc47cd33de368bf660771a179e
Diep Quynh <remilia.1505@gmail.com>
2022-04-12 22:19:07 +0700

    controller: feedback: Add feedback controller
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

---
+++ QuanLyKhachSan/Models/Responses/PhanHoiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyKhachSan.Models.Responses
+{
+    public class PhanHoiResponse
+    {
+        public string TenTK { get; set; }
+        public string NoiDung { get; set; }
+    }
+}
